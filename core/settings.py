6397bc7aba6065d4f53ff59be354e8987f11d463
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2023-09-20 15:49:33 +0700

    remove browser reload for dev purpose

--- core/settings.py
+++ core/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "chatgora",
-    "django_browser_reload",
     "corsheaders",
 ]
 
@@ -58,7 +57,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
     "corsheaders.middleware.CorsMiddleware",
 ]
 
--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,4 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("chatgora.urls")),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
