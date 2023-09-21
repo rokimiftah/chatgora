3c44ab760c65da97b7fe546692e5a516327866d9
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2023-09-21 08:57:48 +0700

    feat: handle user joined

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,7 +1,7 @@
 const APP_ID = "66267224dc0e484b89fd3be7e3626f09";
 const CHANNEL = "main";
 const TOKEN =
-  "007eJxTYPj5mO/HN8HkB590TMV+lZg5BxjKn5cwLP/6Q3W1xOwf+2cpMJiZGZmZGxmZpCQbpJpYmCRZWKalGCelmqcaAyXSDCxnXOZMbQhkZFDc+4eJkQECQXwWhtzEzDwGBgCFSyAl";
+  "007eJxTYFjHNet9lqRWgkTWFROhgORlT2r1Xj/TbX60okViRkC7xzYFBjMzIzNzIyOTlGSDVBMLkyQLy7QU46RU81RjoESageXsOdypDYGMDOaSggyMUAjiszDkJmbmMTAAAM/ZHPI=";
 let UID;
 
 const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
@@ -9,7 +9,8 @@ const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 let localTracks = [];
 let remoteUsers = {};
 
-(joinAndDisplayLocalStream = async () => {
+let joinAndDisplayLocalStream = async () => {
+  client.on("user-published", handleUserJoined);
   UID = await client.join(APP_ID, CHANNEL, TOKEN, null);
 
   localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
@@ -30,4 +31,37 @@ let remoteUsers = {};
   localTracks[1].play(`user-${UID}`);
 
   await client.publish([localTracks[0], localTracks[1]]);
-})();
+};
+
+let handleUserJoined = async (user, mediaType) => {
+  remoteUsers[user.uid] = user;
+  await client.subscribe(user, mediaType);
+
+  if (mediaType === "video") {
+    let player = document.getElementById(`user-container-${user.uid}`);
+
+    if (player != null) {
+      player.remove();
+    }
+
+    player = `
+          <div id="user-container-${user.uid}" class="video-container">
+            <div class="username-wrapper">
+              <span class="user-name">My Name</span>
+            </div>
+            <div id="user-${user.uid}" class="video-player"></div>
+          </div>
+    `;
+
+    document
+      .getElementById("video-streams")
+      .insertAdjacentHTML("beforeend", player);
+    user.videoTrack.play(`user-${user.uid}`);
+  }
+
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+};
+
+joinAndDisplayLocalStream();
